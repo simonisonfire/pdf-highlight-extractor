b19ce737e6b30b4658868079581941e824d7e96b
simonisonfire <136038801+simonisonfire@users.noreply.github.com>
2023-11-29 09:59:36 -0300

    working version

--- pdf-highlight-extractor.py
+++ pdf-highlight-extractor.py
@@ -1,31 +1,27 @@
-import fitz  # Importar PyMuPDF
+pip install PyMuPDF
 
-def extraer_subrayados(pdf_path, txt_path):
-    # Abrir el archivo PDF
+import fitz  # PyMuPDF
+
+def extract_highlighted_text(pdf_path, txt_path):
     pdf_document = fitz.open(pdf_path)
-
-    # Crear un archivo de texto para escribir los fragmentos subrayados
     with open(txt_path, 'w', encoding='utf-8') as txt_file:
         for page_num in range(pdf_document.page_count):
             page = pdf_document.load_page(page_num)
-            annots = page.get_text("dict", clip=page.rect, flags=fitz.TEXT_DEHYPHENATE)  # Obtener las anotaciones/subrayados
-
-            if annots:
-                txt_file.write(f"Page {page_num + 1}:\n")
-                for annot in annots['blocks']:
-                    for line in annot['lines']:
-                        for span in line['spans']:
-                            # Verificar si el color del texto es diferente al color estándar del PDF
-                            if span['color'] != (0, 0, 0):  # Cambiar (0, 0, 0) por el color estándar si es diferente
-                                txt = span['text']
-                                txt_file.write(f"{txt}\n")
+            annotations = page.annots()
+            if annotations:
+                for annot in annotations:
+                    if annot.type[0] == 8:  # Verificar si es un subrayado
+                        rect = annot.rect
+                        x0, y0, x1, y1 = rect
+                        highlights = page.get_text("text", clip=(x0, y0, x1, y1))
+                        if highlights:
+                            txt_file.write(f"{highlights.strip()}\n") # ({page_num + 1}) para el número de página
+    pdf_document.close()
 
 if __name__ == "__main__":
-    # Rutas de entrada y salida
-    pdf_input_path = "ruta/del/archivo.pdf"
-    txt_output_path = "ruta/del/archivo_de_texto.txt"
+    pdf_input_path = "C:/Users/Acer/Downloads/motivacion.pdf"
+    txt_output_path = "C:/Users/Acer/Downloads/motivacion.txt"
 
-    # Llamar a la función para extraer subrayados
-    extraer_subrayados(pdf_input_path, txt_output_path)
+    extract_highlighted_text(pdf_input_path, txt_output_path)
 
     print("Extracción completada. Los subrayados se han guardado en:", txt_output_path)
